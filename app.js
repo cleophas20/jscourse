2a7f8951b3a68621e889a3f095435820bcd6cc10
Cleophas <cleophasfoy@gmail.com>
2022-10-19 13:50:43 -0500

    created index.html, style.css, app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+let message = 'fuck you';
+
+console.log(message);
+
+//operators
+food = Number(prompt('How much did your meal cost?'));
+tipPercentage = Number(prompt('What percentage do you want to tip?'));
+tipDecimal = tipPercentage * 0.01;
+
+tipAmount = food * tipDecimal;
+console.log('tip amount: ' + tipAmount);
+
+total = food + tipAmount;
+
+console.log('total: ' + total);
+
+// user input
