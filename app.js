a1c423eb6b4fbd48c7dd9b90f388a42bb5d1a442
Cleophas <cleophasfoy@gmail.com>
2022-10-20 11:34:00 -0500

    more work on arrays

--- app.js
+++ app.js
@@ -1,17 +1,88 @@
-let message = 'fuck you';
+// let message = 'fuck you';
+//
+// console.log(message);
+//
+// function sum(a, b) {
+//   const sum = a + b
+//   return sum
+// }
+//
+// //operators
+// function calculateFoodTotal(food, tip) {
+//   const tipDecimal = tip * 0.01;
+//   const tipAmount = food * tipDecimal;
+//   const total = sum(food, tipAmount);
+//   return total
+// }
+//
+// console.log(calculateFoodTotal(100, 20));
+//
+// const add = (a, b) => a + b
+// const sub = (a, b) => a - b
+// const mult = (a, b) => a * b
+// const div = (a, b) => {
+//   if (b!== 0) {
+//     return a / b
+//   } console.log('Cannot divide by 0')
+// }
+//
+// console.log(mult(33, 10))
 
-console.log(message);
+// Arrays
 
-//operators
-food = Number(prompt('How much did your meal cost?'));
-tipPercentage = Number(prompt('What percentage do you want to tip?'));
-tipDecimal = tipPercentage * 0.01;
+const fruits = ['apple', 'strawberry', 'banana', 'orange']
 
-tipAmount = food * tipDecimal;
-console.log('tip amount: ' + tipAmount);
+fruits.push('watermelon', 'mango')
 
-total = food + tipAmount;
+//console.log(fruits.slice(3, 5))
 
-console.log('total: ' + total);
+//array methods (slice, push, indexOf, length)
+//console.log(fruits.length)
 
-// user input
+
+// const person = {
+//   name: 'Leonardo',
+//   shirt: 'white',
+//   incoming: 24,
+//   outgoing: 12,
+//   chocolateBars: function () {
+//     return this.incoming - this.outgoing
+//   }
+// }
+//
+// const person2 = {
+//   name: 'Jerry',
+//   shirt: 'blue'
+// }
+//
+// const intro = (guy) => {
+//   const introSentence = `Hi my name is ${guy.name}, and my shirt is ${guy.shirt}, and I have ${guy.chocolateBars()}.`;
+//   return introSentence
+// }
+//
+// console.log(intro(person))
+// for (let i = 0; i < numbers.length, i++) {
+//   sum = numbers[i] + numbers[i+1]
+//   sum = numbers[0]
+// }
+
+
+const numbers = [1, 2, 3, 4, 5, 6]
+let sum = 0
+let doubles = []
+for (num of numbers) {
+//  sum = sum + num
+  doubles.push(num * 2)
+
+}
+//  console.log(doubles)
+
+const letterCounter = (word) => {
+  let count = 0
+  for (letter in word) {
+    console.log(letter)
+  }
+}
+
+let word = prompt('Write your phrase')
+letterCounter(word)
---
+++ weatherapp.js
@@ -0,0 +1,13 @@
+//Baby version
+//
+// let weather = prompt('How is the weather?');
+// if (weather == 'rainy') {
+//   console.log('umbrella');
+// } console.log('sunglasses');
+//
+// function sayMyName() {
+//   let name = prompt('What is your name?')
+//   console.log(name);
+// }
+
+//sayMyName()
